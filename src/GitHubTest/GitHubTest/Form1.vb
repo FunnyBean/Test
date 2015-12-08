062e1d472251e74d8b58776250475dadeddd6a95
Gabrielko <gabrielko@azet.sk>
2015-12-09 00:19:35 +0100

    Pridaná metóda SayHello bez parametrov.

--- src/GitHubTest/GitHubTest/Form1.vb
+++ src/GitHubTest/GitHubTest/Form1.vb
@@ -1,17 +1,21 @@
 ﻿Public Class Form1
 
+    Private Sub SayHello()
+        Me.SayHello(String.Format("{0} {1}", Me.txtName.Text, Me.txtLastName.Text))
+    End Sub
+
     Private Sub SayHello(name As String)
         Dim hw As New HelloWorld
         hw.SayHelloToWorld(name)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.SayHello(String.Format("{0} {1}", Me.txtName.Text, Me.txtLastName.Text))
+        Me.SayHello()
     End Sub
 
     Private Sub txtName_KeyDown(sender As Object, e As KeyEventArgs) Handles txtName.KeyDown, txtLastName.KeyDown
         If e.KeyCode = Keys.Enter Then
-            Me.SayHello(String.Format("{0} {1}", Me.txtName.Text, Me.txtLastName.Text))
+            Me.SayHello()
         End If
     End Sub
 
