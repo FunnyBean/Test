867cb32a0d901701f388d6c40b3fa108af07de99
Gabrielko <gabrielko@azet.sk>
2015-12-09 00:18:34 +0100

    Text sa zadáva cez parameter.

--- src/GitHubTest/GitHubTest/Form1.vb
+++ src/GitHubTest/GitHubTest/Form1.vb
@@ -1,17 +1,17 @@
 ﻿Public Class Form1
 
-    Private Sub SayHello()
+    Private Sub SayHello(name As String)
         Dim hw As New HelloWorld
-        hw.SayHelloToWorld(String.Format("{0} {1}", Me.txtName.Text, Me.txtLastName.Text))
+        hw.SayHelloToWorld(name)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.SayHello()
+        Me.SayHello(String.Format("{0} {1}", Me.txtName.Text, Me.txtLastName.Text))
     End Sub
 
     Private Sub txtName_KeyDown(sender As Object, e As KeyEventArgs) Handles txtName.KeyDown, txtLastName.KeyDown
         If e.KeyCode = Keys.Enter Then
-            Me.SayHello()
+            Me.SayHello(String.Format("{0} {1}", Me.txtName.Text, Me.txtLastName.Text))
         End If
     End Sub
 
